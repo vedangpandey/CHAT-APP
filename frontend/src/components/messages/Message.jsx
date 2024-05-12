4afdc6a81892093fe023371565656c2e77aa3bc4
Vedang <vedangssjgod@gmail.com>
2024-05-12 21:54:20 +0530

    Frontend developed

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+      <div className="chat chat-end">
+      <div className="chat-image avatar">
+        < div className="w-10 rounded-full">
+          <img alt="Tailwind CSS chat bubble component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+  </div>
+  <div className={`chat-bubble text-white  pb-2`}>Hi What is up</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+  </div>
+  )
+};
+
+export default Message

511f60417eed5026b7cf2044889250a873cb08ad
Vedang <vedangssjgod@gmail.com>
2024-05-31 01:37:28 +0530

    Straight to production

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider=({ children })=>{
 	const {authUser} = useAuthContext();
 	useEffect(()=>{
 		if(authUser){
-			const socket=io("http://localhost:5000",{
+			const socket=io("https://chat-app-lo2p.onrender.com/",{
 				query:{
 					userId: authUser._id,
 				}
